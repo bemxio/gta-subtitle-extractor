14b3a6d8430de3f182af333f486eab1b9951c433
bemxio <bemxiov@protonmail.com>
2022-11-05 01:46:15 +0100

    add all of the project's files

---
+++ README.md
@@ -0,0 +1,15 @@
+# gta-subtitle-extractor
+A simple script, written in Python, for converting an '.xml' file with the subtitles' time and duration, and an '.oxt' file with the actual subtitle content, retrieved from the [GTA V's](https://en.wikipedia.org/wiki/Grand_Theft_Auto_V) files, using [OpenIV](https://openiv.com/).
+
+It was tested for [Princess Robot Bubblegum](https://gta.fandom.com/wiki/Princess_Robot_Bubblegum) only, however it should work for other movies/cutscenes in the game.
+
+## Running
+Make sure you're running Python 3.7+ before doing any steps.
+
+1. Clone the repository into a directory of your choice.
+2. Move to the directory with the files in a terminal.
+3. Run `python3 -m pip install -r requirements.txt` to install required requirements.
+4. Done! To check the usage of the script, do `python3 main.py -h`.
+
+## Contributing
+As with all my projects, contributions are highly appreciated!
---
+++ main.py
@@ -0,0 +1,66 @@
+import xml.etree.ElementTree as ET
+from argparse import ArgumentParser
+from datetime import timedelta
+from typing import Optional
+from pathlib import Path
+
+import srt
+
+def main(oxt_path: Path, xml_path: Path, output: Optional[Path] = None):
+    # parse the .oxt text table and make a dict with the text bindings
+    with open(oxt_path, "r", encoding="utf-8") as file:
+        texttable = file.read()
+
+    texttable = texttable.splitlines()
+    bindings = {}
+
+    for line in texttable:
+        if not line.startswith("\t"):
+            continue
+
+        bind = line[1:].split(" = ")
+        bindings[bind[0]] = bind[1].replace("~z~", "")
+
+    # parse the .xml metadata file containing the time and duration of the text
+    tree = ET.parse(xml_path)
+    root = tree.getroot()
+
+    args = root[1] # pCutsceneEventArgsList
+    events = root[2] # pCutsceneEventList
+
+    subtitles = []
+
+    for event in events:
+        time = float(event[0].get("value"))
+        index = int(event[2].get("value"))
+
+        arg = args[index]
+
+        text = bindings.get(arg[0].text)
+        duration = float(arg[1].get("value"))
+
+        subtitles.append(srt.Subtitle(
+            index=index,
+            start=timedelta(seconds=time),
+            end=timedelta(seconds=time + duration),
+            content=text
+        ))
+
+    # write the subtitles to an output file
+    output = output or xml_path.with_suffix(".srt")
+
+    with open(output, "w", encoding="utf-8") as file:
+        file.write(srt.compose(subtitles))
+
+if __name__ == "__main__":
+    parser = ArgumentParser(description="A script for generating an .srt file out of an .xml & .oxt file, retrieved from GTA V's game files.")
+    
+    parser.add_argument("oxt_path", type=Path, help="The input path for an '.oxt' file.")
+    parser.add_argument("xml_path", type=Path, help="The input path for an '.xml' file.")
+    
+    parser.add_argument("--output", "-o", type=Path,
+        help="The output path for an '.srt' file. Defaults to the path of the '.xml' file.", 
+        default=None
+    )
+
+    main(**vars(parser.parse_args()))
